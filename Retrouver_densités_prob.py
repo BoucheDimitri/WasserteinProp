6f6d408510106fbde2820b5d44dc6094d20178e4
Vincent <45663930+VincentPla@users.noreply.github.com>
2018-12-26 02:22:59 +0100

    Update Retrouver_densités_prob.py

--- Retrouver_densités_prob.py
+++ Retrouver_densités_prob.py
@@ -5,8 +5,14 @@ form scipy.stats import norm
 
 class Obtenir_la_densite_de_probabilitees:
     
-    def __init__(self, pts_discretisation):
-        self.pts_discretisation = pts_discretisation # pts s_i
+    def __init__(self, pts_discretisation, inv_cdf):
+        """
+        Params:
+            pts_discretisation : Set of discretization points
+            inv_cdf : fonction inv_cdf(agent,s)=g_s(v) où s est le point en lequel on calcule
+        """
+        self.pts_discretisation = pts_discretisation
+        self.inv_cdf = inv_cdf
     
     ''' crée un échantillon de nsamples variables tirées par un agent'''
     def draw(self, agent, nsamples=1000):
@@ -14,8 +20,7 @@ class Obtenir_la_densite_de_probabilitees:
         ''' ici self.pts_discretisation = les pts s_i '''
         func = np.vectorize(lambda u : bisect.bisect(self.pts_discretisation, u))
         S = func(U) # tous les points s_i pour lesquels on renvoie (g_{s_i}(v))
-        ''' je ne suis pas certain d'avoir compris comment accèder aux g_s(v) '''
-        return ''' il faut retourner [g_{s}(agent) for s in S] '''
+        return np.array([inv_cdf(agent,s) for s in S])
         
     ''' plutôt dans le cas discret '''
     def methode_MC(self, agent, nsamples=1000):
@@ -31,10 +36,9 @@ class Obtenir_la_densite_de_probabilitees:
         h = sigma/nsamples**(0.2) # thumb rule
         densite = lambda y : np.sum([K((y-x)/h) for x in echantillon])/(n*h)
         return densite
-        
+    
     ''' pour la méthode des différences finies, on retourne une fonction mais on peut l'adapter pour renvoyer : ensemble_des_valeurs, prob '''
     def methode_differences_finies(self, agent, nsamples=1000):
-        ''' je ne suis pas certain d'avoir compris comment accèder aux g_s(v), c'est pourquoi je suppose que inv_cdf(agent) = (g_{s_1}(agent),...,g_{s_n}(agent)), mais il faudra remplacer '''
         derivee = (inv_cdf(agent)[1:]-inv_cdf(agent)[:-1])/(nsamples-1)
         ''' regarder s'il y a une méthode plus efficace pour les listes triées dans l'ordre croissant '''
         densite = lambda y : 1/derivee[bisect.bisect(inv_cdf(agent), y)]
@@ -42,7 +46,6 @@ class Obtenir_la_densite_de_probabilitees:
     
     ''' plutôt dans le cas discret '''
     def methode_fct_repartition(self, agent, nsamples):
-        ''' je ne suis pas certain d'avoir compris comment accèder aux g_s(v), c'est pourquoi je suppose que inv_cdf(agent) = (g_{s_1}(agent),...,g_{s_n}(agent)), mais il faudra remplacer '''
         cdf_inv_bis, uniq_inv = np.unique(inv_cdf(agent), unique_inverse=True)
         ensemble_des_valeurs = (cdf_inv_bis[1:] + cdf_inv_bis[;-1])/2
         S = self.pts_discretisation[uniq_inv]
