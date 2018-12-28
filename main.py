1977d613143e69df871a0d31fe962e947b771991
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-12-28 12:19:37 +0100

    Added plotting to monitor convergence

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ importlib.reload(anet)
 
 # Create empty InvCdf instances
 invcdf1 = icdf.InvCdf(nsamples_icdf=10000)
-invcdf2 = icdf.InvCdf()
-invcdf3 = icdf.InvCdf()
-invcdf4 = icdf.InvCdf()
+invcdf2 = icdf.InvCdf(nsamples_icdf=10000)
+invcdf3 = icdf.InvCdf(nsamples_icdf=10000)
 
 
 # Fill them with beta distributions
@@ -22,61 +21,63 @@ invcdf4 = icdf.InvCdf()
 a1, b1 = 0.5, 0.5
 a2, b2 = 0.1, 0.8
 a3, b3 = 0.8, 0.1
-a4, b4 = 0.4, 0.6
 # Instantiate the beta distributions
 dist1 = stats.beta(a1, b1)
 dist2 = stats.beta(a2, b2)
 dist3 = stats.beta(a3, b3)
-dist4 = stats.beta(a4, b4)
-
-
-
-p1 = 0.1
-p2 = 0.3
-p3 = 0.5
-p4 = 0.9
-
-dist1 = stats.bernoulli(p1)
-dist2 = stats.bernoulli(p2)
-dist3 = stats.bernoulli(p3)
-dist4 = stats.bernoulli(p4)
-
-
-
 
 
 # Fill InvCdf instances with the beta distribution
 invcdf1.fill_from_scipy(dist1)
 invcdf2.fill_from_scipy(dist2)
 invcdf3.fill_from_scipy(dist3)
-invcdf4.fill_from_scipy(dist4)
 # Plot the invcdfs
 plt.figure()
-plt.plot(invcdf1.qs, invcdf1.y, label=0)
-plt.plot(invcdf1.qs, invcdf2.y, label=1)
-plt.plot(invcdf1.qs, invcdf3.y, label=2)
-plt.plot(invcdf1.qs, invcdf4.y, label=3)
+plt.plot(invcdf1.qs, invcdf1.invcdf, label=0)
+plt.plot(invcdf1.qs, invcdf2.invcdf, label=1)
+plt.plot(invcdf1.qs, invcdf3.invcdf, label=2)
 plt.legend()
 plt.title("Initial inverse cdfs")
 
 
 # Create agent network
 # Number of agents in the network
-npeers = 4
+npeers = 3
 # Initialize neighbors
 # Initialize agents with the beta distributions as model sol
 agent1 = anet.Agent(npeers, 0, invcdf1)
 agent2 = anet.Agent(npeers, 1, invcdf2)
 agent3 = anet.Agent(npeers, 2, invcdf3)
-agent4 = anet.Agent(npeers, 3, invcdf4)
 # Stack agents in a list
-agents = [agent1, agent2, agent3, agent4]
+agents = [agent1, agent2, agent3]
+
 # Initialize weight matrix (here we take a complete graph)
 W = np.ones((npeers, npeers)) - np.eye(npeers)
 # Confidence of 1 in every model
 C = np.ones(npeers)
+
 # Build network
 network = anet.AgentNetwork(W, C, agents, mu=1)
+
+# Monitor iteration of asynchronous gossip algorithm
+k1 = 3
+k2 = 3
+fig, axes = plt.subplots(nrows=k1, ncols=k2)
+for t in range(0, k1 * k2):
+    if t == 0:
+        for i in range(0, npeers):
+            axes[0, 0].plot(invcdf1.qs, network.agents[i].models_matrix[:, i], label=i)
+        axes[0, 0].legend()
+        axes[0, 0].set_title("t = 0")
+    else:
+        network.async_gossip_step()
+        for i in range(0, npeers):
+            axes[t // k1, t % k2].plot(invcdf1.qs, network.agents[i].models_matrix[:, i], label=i)
+        axes[t // k1, t % k2].legend()
+        axes[t // k1, t % k2].set_title("t = " + str(t))
+
+
+
 # Run asynchronous gossip algorithm
 network.iterate_async_gossip(100)
 # Update finale model for each agents
